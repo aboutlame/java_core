75c52ec5cdf1fb31b6fb905e94eaef4877ddad77
Тимошенко Дмитрий <palacehellot@gmail.com>
2025-10-17 21:56:42 +0300

    MP - 18

---
+++ src/main/java/ru/mentee/power/variables/First.java
@@ -0,0 +1,10 @@
+package main.java.ru.mentee.power.variables;
+
+public class First {
+    final static int myAge = 19;
+    final static String myName = "Дима";
+
+    public static void main(String[] args) {
+        System.out.println("Меня зовут " + myName + ", мне " + myAge + " лет.");
+    }
+}
